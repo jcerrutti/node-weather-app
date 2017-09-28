89154cec0aad106a4dd152775b6001c7e0562c6b
unknown <jicerrutti@gmail.com>
2017-09-28 12:21:39 -0300

    Some examples request

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
-
+var API_KEY = 'AIzaSyAiFV7Fl-yTWvgAhH6a_HATuE7CbYSN-y0'
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key='+API_KEY,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 })
