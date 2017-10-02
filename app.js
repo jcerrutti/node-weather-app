e952212266b378a31aa409f8b57b004ee8f9e69c
juan.cerrutti <juan.cerrutti@darwoft.com>
2017-10-02 11:12:42 -0300

    Fetching address and getting actual weather

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const yargs = require('yargs');
-const geocode = require('./geocode/geocode.js')
 const request = require('request');
+
+const geocode = require('./geocode/geocode')
+const weather = require('./weather/weather')
 
 const argv = yargs
     .options({
@@ -19,22 +21,20 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
     if (errorMessage) {
         console.log(errorMessage);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weather) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`It's currently ${weather.temperature}. It feels like ${weather.apparentTemperature}`);
+            }
+        });
     }
 });
+
 
 
 //495234615dffecf93cd6485850b07874
 
 //https://api.darksky.net/forecast/495234615dffecf93cd6485850b07874/-31.34569419999999,-64.3365754
 
-request({
-    url: 'https://api.darksky.net/forecast/495234615dffecf93cd6485850b07874/-31.34569419999999,-64.3365754?units=si',
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(body.currently.temperature);
-    } else {
-        console.log('Unable to fetch weather');
-    }
-})
---
+++ weather/weather.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+var getWeather = (lat, lng, callback) => {
+
+
+    request({
+        url: `https://api.darksky.net/forecast/495234615dffecf93cd6485850b07874/${lat},${lng}?units=si`,
+        json: true
+    }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature
+            });
+        } else {
+            callback('Unable to fetch weather');
+        }
+    })
+
+}
+
+module.exports = {
+    getWeather
+}
