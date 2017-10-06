aae590161f51de74c7233269f99bbc0f7269f760
juan.cerrutti <juan.cerrutti@darwoft.com>
2017-10-06 15:52:52 -0300

    Deleted original app file

--- app-promise.js
+++ app-promise.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const axios = require('axios');
+const iplocation = require('iplocation');
 
 const GEOCODE_API_KEY = 'AIzaSyAiFV7Fl-yTWvgAhH6a_HATuE7CbYSN-y0';
 const WEATHER_API_KEY = '495234615dffecf93cd6485850b07874'
@@ -7,7 +8,7 @@ const WEATHER_API_KEY = '495234615dffecf93cd6485850b07874'
 const argv = yargs
     .options({
         a: {
-            demand: true,
+            demand: false,
             alias: 'address',
             describe: 'Address to fetch weather for',
             string: true
@@ -17,18 +18,25 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-const encodeLocation = encodeURIComponent(argv.address);
-const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeLocation}&key=${GEOCODE_API_KEY}`;
-
-axios.get(geocodeUrl).then((response) => {
-    if (response.data.status === 'ZERO_RESULTS') {
-        throw new Error('Unable to find that address.');
+var location = () => {
+    if (argv.address) {
+        const encodeLocation = `https://maps.googleapis.com/maps/api/geocode/json?address=$${argv.address}&key=${GEOCODE_API_KEY}`;
+        return axios.get(encodeLocation);
+    } else {
+        return iplocation().then((address) => {
+            const ipAddress = `${address.region_name}, ${address.country_name}`;
+            const encodeLocation = `https://maps.googleapis.com/maps/api/geocode/json?address=$${ipAddress}&key=${GEOCODE_API_KEY}`;
+            return axios.get(encodeLocation);
+        })
     }
+}
+
+location().then((response) => {
+    console.log(response.data.results[0].formatted_address);
     const lat = response.data.results[0].geometry.location.lat;
     const lng = response.data.results[0].geometry.location.lng;
     const weatherUrl = `https://api.darksky.net/forecast/${WEATHER_API_KEY}/${lat},${lng}?units=si`;
-    console.log(response.data.results[0].formatted_address);
-    return axios.get(weatherUrl);
+    return axios.get(weatherUrl)
 }).then((response) => {
     const temperature = response.data.currently.temperature;
     const apparentTemperature = response.data.currently.apparentTemperature;
@@ -37,6 +45,7 @@ axios.get(geocodeUrl).then((response) => {
     if (error.code === 'ENOTFOUND') {
         console.log('Unable to connect to API servers')
     } else {
+        console.log('error');
         console.log(error.message);
     }
-})
+});
